84f0c5b5d6c28f45f138b29b48d1cee4144a5604
k4v4l3r4 <bram.newbie@gmail.com>
2025-12-02 12:34:39 +0700

    first commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,4 +6,6 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: false,
+  token: process.env.SANITY_API_READ_TOKEN,   // 👈 WAJIB
+  perspective: "published",                    // 👈 BIAR DATA PUBLISHED MUNCUL
 })
