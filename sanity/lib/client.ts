bcd0e0b73db7c7e867411c479280f0915191d060
k4v4l3r4 <bram.newbie@gmail.com>
2025-12-02 12:43:24 +0700

    first commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,17 +1,9 @@
-// FILE: sanity/lib/client.ts
-
 import { createClient } from "next-sanity"
 import { apiVersion, dataset, projectId } from "@/sanity/env"
-
-// ===============================
-//  SANITY CLIENT FINAL & AMAN
-// ===============================
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false,                 // gunakan data langsung
-  token: process.env.SANITY_API_READ_TOKEN,  // WAJIB untuk production
-  perspective: "published",      // hanya ambil data publish, aman di frontend
+  useCdn: true,    // pake CDN supaya public-safe
 })
