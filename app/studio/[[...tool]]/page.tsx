73e1c58239b660e717372e059125645285373e8b
k4v4l3r4 <bram.newbie@gmail.com>
2025-12-10 18:21:06 +0700

    update

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,13 +1,7 @@
-/**
- * Route untuk Sanity Studio di Next.js App Router.
- * Semua path di bawah /studio akan di-handle oleh file ini.
- */
+'use client'
 
 import { NextStudio } from "next-sanity/studio"
 import config from "../../../sanity.config"
-
-export const dynamic = "force-static"
-export { metadata, viewport } from "next-sanity/studio"
 
 export default function StudioPage() {
   return <NextStudio config={config} />
